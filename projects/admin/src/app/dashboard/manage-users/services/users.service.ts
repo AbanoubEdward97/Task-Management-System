a2e612ea6bd52656020d2200f3b3292c73584af6
AbanoubEdward <abanoub.edward97101@gmail.com>
2024-01-22 13:57:31 +0200

    initiate users service

--- projects/admin/src/app/dashboard/manage-users/services/users.service.ts
+++ projects/admin/src/app/dashboard/manage-users/services/users.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'projects/admin/src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  getAllUsers(filter:any){
+    return this.http.get(environment.baseApi.replace('tasks','auth') + '/users')
+  }
+  deleteUser(id:string){
+    return this.http.delete
+    
+  }
 }
